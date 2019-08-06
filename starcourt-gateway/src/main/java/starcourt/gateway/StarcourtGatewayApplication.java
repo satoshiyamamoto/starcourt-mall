c5fd5ae3b29b5b92e97c366d2844247e44cab764
Satoshi Yamamoto <satoshiyamamoto@me.com>
2019-08-07 08:53:02 +0900

    Added Hystrix Dashboard.

--- starcourt-gateway/src/main/java/starcourt/gateway/StarcourtGatewayApplication.java
+++ starcourt-gateway/src/main/java/starcourt/gateway/StarcourtGatewayApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableTurbine
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class StarcourtGatewayApplication {
 
