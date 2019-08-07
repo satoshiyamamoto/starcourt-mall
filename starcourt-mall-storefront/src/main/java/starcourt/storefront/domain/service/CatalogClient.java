0df52a9f8b4c7d17cd71eb05803fd7bd2b1ba88b
Satoshi Yamamoto <satoshiyamamoto@me.com>
2019-08-07 15:38:11 +0900

    Rename module prefix to starcourt-mall-* and update spring-boot version.

---
+++ starcourt-mall-storefront/src/main/java/starcourt/storefront/domain/service/CatalogClient.java
@@ -0,0 +1,27 @@
+package starcourt.storefront.domain.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.client.RestTemplate;
+import starcourt.storefront.domain.model.Product;
+
+@RibbonClient("starcourt-mall-catalog")
+public class CatalogClient {
+
+    private final RestTemplate restTemplate;
+
+    public CatalogClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @HystrixCommand(fallbackMethod = "getProductFallback")
+    public Product getProduct(Long id) {
+        return restTemplate.getForObject("http://starcourt-mall-catalog/{id}", Product.class, id);
+    }
+
+    public Product getProductFallback(Long id, Throwable throwable) {
+        Product product = new Product();
+        product.setId(id);
+        return product;
+    }
+}
---
+++ starcourt-mall-storefront/src/main/java/starcourt/storefront/domain/service/ReviewClient.java
@@ -0,0 +1,42 @@
+package starcourt.storefront.domain.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.Data;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import starcourt.storefront.domain.model.Product.Review;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@RibbonClient("starcourt-mall-review")
+public class ReviewClient {
+
+    private final RestTemplate restTemplate;
+
+    public ReviewClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @HystrixCommand(fallbackMethod = "getReviewFallback")
+    public List<Review> getReview(Long id) {
+        ResponseEntity<Page<Review>> response = restTemplate.exchange("http://starcourt-mall-review/?productId={id}",
+                HttpMethod.GET, null, new ParameterizedTypeReference<Page<Review>>() {}, id);
+        return Objects.requireNonNull(response.getBody()).getContent();
+    }
+
+    public List<Review> getReviewFallback(Long id, Throwable throwable) {
+        return Collections.emptyList();
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class Page<T> {
+        private List<T> content;
+    }
+}
